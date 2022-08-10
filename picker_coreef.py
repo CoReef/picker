85e700f0407be6b894a52d8ec7221fbbf78cb8e8
Peter Sturm <sturm@uni-trier.de>
2022-08-10 17:07:35 +0200

    Processing messages in sequence

--- picker_coreef.py
+++ picker_coreef.py
@@ -1,28 +1,87 @@
 import socket
 import struct
 import json
+import time
+import os
+import argparse
+
+from datetime import datetime
+from datetime import timezone
 
 multicast_group = '239.255.0.42'
-
 server_address = ('', 4242)
+backlog_size = 5
+write_frequency = 2
+
+devices = {}
+
+def new_device (d,address):
+    initial = {
+        "name":d['device'],
+        "address":address,
+        "last_seq":d['sequence']-1,
+        "r_timebase":d['current'],
+        "l_timebase":time.time(),
+        "last_seen":time.time(),
+        "mcount":0,
+        "readings":[],
+        "not_written":0
+        }
+    return initial
+
+def write_data_to_file(dir,d):
+    dt = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+    p = os.path.join(dir,f"{dt}_{d['name']}_data.json")
+    with open(p,'w') as fd:
+        fd.write(json.dumps(d,indent=4))
+
+def process_message (d,message):
+    d['last_seen']=time.time()
+    if d['last_seq'] == message['sequence']:
+        return
+    if message['sequence'] == d['last_seq']+1:
+        d['last_seq'] += 1
+        d['mcount'] += 1
+        timestamp = datetime.now().strftime('%Y-%m-%d %H\:%M\:%S')
+        reading = (message['sequence'],time.time(),timestamp,message['current_t'],message['current_h'])
+        d['readings'].append(reading)
+        d['not_written'] += 1
+        print(d)
+    else:
+        print(f'Expecting sequence number <{d["last_seq"]+1}> but received <{message["sequence"]}>')
+
 
 def main():
+    global write_frequency
+    if backlog_size < write_frequency:
+        write_frequency = backlog_size
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--outdir", type=str, required=False, help="The directory to store data files",default=".")
+    args = parser.parse_args()
+
+    # Check for the output directory and create it if not
+    data_dir = os.path.abspath(args.outdir)
+    if not os.path.isdir(data_dir):
+        os.makedirs(data_dir)
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(server_address)
     group = socket.inet_aton(multicast_group)
     mreq = struct.pack('4sL', group, socket.INADDR_ANY)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-    loop_count = 0
     while True:
-        loop_count += 1
-        print(f"Waiting for UDP multicast message <{multicast_group}> ...")
         rawdata, address = sock.recvfrom(1024)
-        print(f"\nLoop {loop_count}: Received {len(rawdata)} bytes from {address}\n")
-        print(f"Content is <{rawdata}>\n")
         data = json.loads(rawdata)
-        print(f"as dict: <{data}>\n")
-        print(f"The current temperature is {data['current_t']} Celsius\n")
-        print(f"Temperature backlog is {data['backlog_t']}\n")
+        device_name = data['device']
+        if not device_name in devices:
+            devices[device_name] = new_device(data,address)
+        process_message(devices[device_name],data)
+        if devices[device_name]['not_written'] == write_frequency:
+            write_data_to_file(data_dir,devices[device_name])
+            devices[device_name]['not_written'] = 0
+        if len(devices[device_name]['readings']) > backlog_size:
+            devices[device_name]['readings'] = devices[device_name]['readings'][1:]
 
 if __name__ == '__main__':
     main()
